a065a96ec6bcb81cb5d9cf8097e7498fa1534ee9
vakhadir <vakhadir@yahoo.com>
2014-07-15 16:02:18 -0700

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -5,21 +5,30 @@
 ## setter and getter function for the Origina Matrix and Inverse Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        
         inverseMatrix <- NULL
+        
+        ## Sets a given matrix y to x
         set<-function(y){
                 x <<- y
                 inverseMatrix <<- NULL
         }
         
+        ## Returns the original matrix
         get <- function(){
-          x      
+                x      
         }        
         
+        ## sets the inversedMatrix to inverseMatrix. 
+        ## Names of both variables are same.But one is function parameter
+        ## Other one is the resultant matrix definded at the beginning of 
+        ## the method makeCacheMatrix.
         setInverse <- function(inverseMatrix){
                 print("Setting to inverse matrix")
                 inverseMatrix<<-inverseMatrix
         }
         
+        ## Returns the resultant Inversed matrix
         getInverse<-function(){
                 inverseMatrix
         }
@@ -29,19 +38,26 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Gets the Inverse Matrix if if is null, computes the inverse of the matrix 
-## then caches the value by calling setInverse function. For subsequent requests,
-## Since, inverse is already computed, it gets it just by calling getInverse function
+## Returns the Inverse Matrix for the given matrix. If inverse is not already computed, 
+## computes the inverse of the matrix 
+## then caches the value by calling setInverse function. 
+## For subsequent requests, since inverse is already computed, 
+## it gets it just calls getInverse function to return the already computed result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## get the inverseMatrix
         inverseMatrix <-x$getInverse()
+        # If already exist return it
         if(!is.null(inverseMatrix)){
                 print("Readly available in Cache... gettin it")
                 return (inverseMatrix)                
         }
+        ## if not exist get the original matrix
         originalMatrix <-x$get()
+        ## inverse it
         inverseMatrix <- solve(originalMatrix)
+        ## after computing inverse, cache it by calling the setInverse function
         x$setInverse(inverseMatrix)
+        ## Finally return the inversed Matrix- the result.
         inverseMatrix
 }
